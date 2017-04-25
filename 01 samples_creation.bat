9a59def8b9f27c5a5af6855764ac7b35f98d33ff
rh01 <1048157315@qq.com>
2017-04-25 11:40:15 +0800

    initial all files

--- 01 samples_creation.bat
+++ 01 samples_creation.bat
@@ -1,1 +1,1 @@
-createsamples.exe -info positive/info.txt -vec vector/vector.vec -num 100000 -w 24 -h 24
+createsamples.exe -info positive/info.txt -vec vector/vector.vec -num 200 -w 24 -h 24
--- 02 haarTraining.bat
+++ 02 haarTraining.bat
@@ -1,3 +1,3 @@
-haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 36 -nneg 270 -nstages 15 -mem 1024 -mode ALL -w 24 -h 24 
+haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 10 -nneg 200 -nstages 15 -mem 1024 -mode ALL -w 24 -h 24 
 
 rem -nonsym
